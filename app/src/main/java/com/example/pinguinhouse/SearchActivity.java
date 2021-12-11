02f32d1c6b779c0dac8c0b9ebdbf54df24fc5d71
Solene <suchuku35@gmail.com>
2021-12-11 21:09:17 +0100

    Add database input from all search categories

--- app/src/main/java/com/example/pinguinhouse/SearchActivity.java
+++ app/src/main/java/com/example/pinguinhouse/SearchActivity.java
@@ -38,9 +38,11 @@ public class SearchActivity extends AppCompatActivity {
                 Instant instant = Instant.now();
                 String output2 = instant.toString();
                 mydb.insertData(output1,output2);
+                Intent intent = new Intent(v.getContext(), ListActivity.class);
+                v.getContext().startActivity(intent);
 
             }
-        });*/
+        } );*/
 
         bAuthor.setOnClickListener(new GetTextOnClickListener(this));
 
@@ -49,6 +51,65 @@ public class SearchActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ListActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
+
+
+        ImageButton bWork = (ImageButton)findViewById(R.id.btnWorkSearch);
+        bWork.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                Log.i("JFL", "in OnClick function");
+                EditText var1 = (EditText) findViewById(R.id.inputWork);
+                Log.i("JFL", String.valueOf(var1.getText()));
+                String output1 = String.valueOf(var1.getText());
+                Log.i("JFL", "output1 = " + output1);
+                Instant instant = Instant.now();
+                String output2 = instant.toString();
+                mydb.insertData(output1, output2);
+                Intent intent = new Intent(v.getContext(), ListActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
+        ImageButton bTitle = (ImageButton)findViewById(R.id.btnTitleSearch);
+        bTitle.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                Log.i("JFL", "in OnClick function");
+                EditText var1 = (EditText) findViewById(R.id.inputTitle);
+                Log.i("JFL", String.valueOf(var1.getText()));
+                String output1 = String.valueOf(var1.getText());
+                Log.i("JFL", "output1 = " + output1);
+                Instant instant = Instant.now();
+                String output2 = instant.toString();
+                mydb.insertData(output1, output2);
+                Intent intent = new Intent(v.getContext(), ListActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+
+
+        ImageButton bEvent = (ImageButton)findViewById(R.id.btnEventSearch);
+        bEvent.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                Log.i("JFL", "in OnClick function");
+                EditText var1 = (EditText) findViewById(R.id.inputEvent);
+                Log.i("JFL", String.valueOf(var1.getText()));
+                String output1 = String.valueOf(var1.getText());
+                Log.i("JFL", "output1 = " + output1);
+                Instant instant = Instant.now();
+                String output2 = instant.toString();
+                mydb.insertData(output1, output2);
                 Intent intent = new Intent(v.getContext(), ListActivity.class);
                 v.getContext().startActivity(intent);
             }
